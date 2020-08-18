db80fc1b8cd0485ff293c49fa04e6678702b8c7f
necket <nik.sherbakov.01@gmail.com>
2020-08-18 19:45:01 +0300

    0.1

---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Storage from './../storage'
+
+class Header extends React.Component {
+    render() {
+        
+        
+
+        return (
+            <div className="container">
+                <div className="header">
+                    <div className="header-top">
+
+                    </div>
+                    <div className="nav">
+                        
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Header;
