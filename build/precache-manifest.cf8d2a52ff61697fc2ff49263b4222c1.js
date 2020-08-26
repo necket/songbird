bfa1762164760fb8ae39f206fe334cac857b252e
necket <nik.sherbakov.01@gmail.com>
2020-08-26 21:34:37 +0300

    build

---
+++ build/precache-manifest.cf8d2a52ff61697fc2ff49263b4222c1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93fb03373a65e9b08155d64dc5877b70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca3fdfe9bde2358cd598",
+    "url": "/static/css/main.1d4c14ab.chunk.css"
+  },
+  {
+    "revision": "a0c7e331c6433622f1aa",
+    "url": "/static/js/2.29364e42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.29364e42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca3fdfe9bde2358cd598",
+    "url": "/static/js/main.eb5dce87.chunk.js"
+  },
+  {
+    "revision": "dc9173b7e8edec60da3a",
+    "url": "/static/js/runtime-main.3ffde9c1.js"
+  },
+  {
+    "revision": "06a469380cb09a64d372f773665e7500",
+    "url": "/static/media/bird.06a46938.jpg"
+  },
+  {
+    "revision": "165166d5c9e027367cb75b2ac89dcfc0",
+    "url": "/static/media/error.165166d5.mp3"
+  },
+  {
+    "revision": "bae7bc77c5468b3ca1e5627191e84480",
+    "url": "/static/media/logo.bae7bc77.png"
+  },
+  {
+    "revision": "a1e9e8b671592af8b3a503b23bbba6eb",
+    "url": "/static/media/win.a1e9e8b6.mp3"
+  }
+]);
